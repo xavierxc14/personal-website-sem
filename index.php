8e1b0e138df823d53aaaa642dea95ac84ab93443
kabrial <kevin.abrial@hotmail.fr>
2017-12-01 16:23:03 +0100

    Test htaccess for PHP.

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php  
+
+$accept = explode(',', $_SERVER['HTTP_ACCEPT']);
+if (in_array('application/rdf+xml', $accept)) {
+    header('Location: path/to/rdf/version.rdf');
+    die();
+}
+else {
+header( 'Location: /index.html' ) ; 
+}
+?>
