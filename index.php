27dbea5061d73da4ae9be7235c1285a1dd349d63
kabrial <kevin.abrial@hotmail.fr>
2017-12-02 00:16:56 +0100

    Test du index.php du redirect 303 pour le turtle sur /.

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 $accept = explode(',', $_SERVER['HTTP_ACCEPT']);
 if (in_array('application/rdf+xml', $accept)) {
-    header('Location: htaccess/index.rdf');
     http_response_code(303);
+    header('Location: htaccess/index.rdf');
     die();
 } elseif (in_array('text/turtle', $accept)) {
-    header('Location: htaccess/index.ttl');
     http_response_code(303);
+    header('Location: htaccess/index.ttl');
     die();	
 }
 else {
