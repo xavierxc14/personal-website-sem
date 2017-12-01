4b96d1f77e2d1e1228ef8a8edd1c8cbff413d8a6
kabrial <kevin.abrial@hotmail.fr>
2017-12-01 16:47:46 +0100

    Add htaccess in index.php.

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 $accept = explode(',', $_SERVER['HTTP_ACCEPT']);
 if (in_array('application/rdf+xml', $accept)) {
     header('Location: htaccess/index.rdf');
+    http_response_code(303);
     die();
 } elseif (in_array('text/turtle', $accept)) {
     header('Location: htaccess/index.ttl');
+    http_response_code(303);
     die();	
 }
 else {
