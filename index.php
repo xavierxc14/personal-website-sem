4b82e950268bf7999d3fa4c79d43eac3cb8ceab8
kabrial <kevin.abrial@hotmail.fr>
2017-12-03 00:15:09 +0100

    Renomage du dossier htaccess en .htaccess.

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<?php  
+<?php
 
 $accept = explode(',', $_SERVER['HTTP_ACCEPT']);
 if (in_array('application/rdf+xml', $accept)) {
     http_response_code(303);
-    header('Location: htaccess/index.rdf');
+    header('Location: .htaccess/index.rdf');
     die();
 } elseif (in_array('text/turtle', $accept)) {
     http_response_code(303);
-    header('Location: htaccess/index.ttl');
-    die();	
+    header('Location: .htaccess/index.ttl');
+    die();
 }
 else {
-header( 'Location: /index.html' ) ; 
+header( 'Location: /index.html' ) ;
 }
 ?>
