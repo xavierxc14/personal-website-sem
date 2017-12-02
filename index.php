81ce0a1bc7ea613ecb33239c82c58741a50f64ad
kabrial <kevin.abrial@hotmail.fr>
2017-12-03 00:18:17 +0100

    Modify index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 $accept = explode(',', $_SERVER['HTTP_ACCEPT']);
 if (in_array('application/rdf+xml', $accept)) {
     http_response_code(303);
-    header('Location: .htaccess/index.rdf');
+    header('Location: htaccess/index.rdf');
     die();
 } elseif (in_array('text/turtle', $accept)) {
     http_response_code(303);
-    header('Location: .htaccess/index.ttl');
+    header('Location: htaccess/index.ttl');
     die();
 }
 else {
