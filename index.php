a53815f50ec0bc3a262854f730f0eef8c953442b
Sangfer <alexis.barthelemy02@gmail.com>
2017-12-05 15:00:32 +0100

    try get automatic turtle page when changing ACCEPT

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 
 $accept = explode(',', $_SERVER['HTTP_ACCEPT']);
+$pagename=basename($_SERVER['PHP_SELF'],'.php')
 if (in_array('application/rdf+xml', $accept)) {
     http_response_code(303);
-    header('Location: htaccess/index.rdf');
+    header('Location: htaccess/'.$pagename.'.rdf');
     die();
 } elseif (in_array('text/turtle', $accept)) {
     http_response_code(303);
-    header('Location: htaccess/index.ttl');
+    header('Location: htaccess/'.$pagename.'.ttl');
     die();
 }
 else {
